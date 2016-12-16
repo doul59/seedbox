369dbc0da34f4763f257a2eb95d4351c8b9c88d4
doul59 <doul59@msn.com>
2016-12-16 15:41:00 +0100

    Update startDownloadSuite.sh

--- startDownloadSuite.sh
+++ startDownloadSuite.sh
@@ -3,5 +3,5 @@
 python /root/CouchPotatoServer/CouchPotato.py --daemon
 python /root/SickRage/SickBeard.py --nolaunch --daemon
 /usr/bin/deluged -d &
-/usr/bin/deluge-web -d -p 8080 &
+/usr/bin/deluge-web -p 8080 &
 exec /bin/bash
