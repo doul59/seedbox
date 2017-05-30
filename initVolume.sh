10831fc9ca1868a5dbecf4b3ecc55ee2ad36c3c5
doul59 <doul59@msn.com>
2017-05-30 20:47:16 +0200

    Update initVolume.sh

--- initVolume.sh
+++ initVolume.sh
@@ -6,20 +6,17 @@ cp -f /remount.sh /root/remount.sh
 mkdir -p /root/acd
 #/root/remount.sh &>/dev/null|| { echo "ERROR : Could not mount ACD. Make sure your volume has .cache/acd/oauth_data file" ; exit 1; }
 /root/remount.sh
-mkdir -p /root/acd/temp
-mkdir -p /root/acd/Shows
-mkdir -p /root/acd/Movies
-mkdir -p /root/.cache/acd_cli/
+mkdir -p /mnt/gcd_1
+mkdir -p /media/gcd_1
 mkdir -p /root/.couchpotato
 mkdir -p /root/.config/deluge
 cp -n /couchpotato_settings.conf /root/.couchpotato/settings.conf
 cp -n /sickrage_config.ini /root/SickRage/config.ini
 cp -n /deluge_config.conf /root/.config/deluge/core.conf
-mkdir -p /root/downloads/completed
+mkdir -p /root/downloads/completed/Shows
+mkdir -p /root/downloads/completed/Movies
 mkdir -p /root/downloads/incomplete
 mkdir -p /root/downloads/torrents
-ln -s /root/acd/temp ~/downloads/Shows &>/dev/null
-ln -s /root/acd/Movies ~/downloads/Movies  &>/dev/null
 cp -f /startDownloadSuite.sh /root/startDownloadSuite.sh
 test -f /root/SickRage/_init || cp  -f /sickrage_config.ini ~/SickRage/config.ini
 touch /root/SickRage/_init
