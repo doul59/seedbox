c53aef4b279f93ec8513bb42d2d24e73782a25c3
doul59 <doul59@msn.com>
2017-05-30 20:53:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,14 @@ apt-get update
 apt-get install -y python
 apt-get install -y python3
 apt-get install -y python3-pip
+apt-get install -y curl
+apt-get install -y wget
+apt-get install -y unzip
 apt-get install -y git
 apt-get install -y fuse
 apt-get install -y nano
 apt-get install -y vim
+apt-get install -y encfs
 apt-get install -y unzip unrar bzip2
 debconf-set-selections <<< "postfix postfix/mailname string seedbox"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
@@ -17,10 +21,18 @@ apt-get install -y postfix
 apt-get install -y procmail
 apt-get install deluged -qy
 apt-get install deluge-webui -qy
+#rclone
 cd /root
-git clone https://github.com/yadayada/acd_cli.git &>/dev/null
-cd acd_cli && python3 setup.py install &>/dev/null
-#Fixes DECRYPTION_FAILED_OR_BAD_RECORD_MAC
-apt-get install -y wget
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip &>/dev/null
+unzip rclone-current-linux-amd64.zip &>/dev/null
+cd rclone-*-linux-amd64
+cp rclone /usr/bin/
+chown root:root /usr/bin/rclone
+chmod 755 /usr/bin/rclone
+mkdir -p /usr/local/share/man/man1
+cp rclone.1 /usr/local/share/man/man1/
+mandb &>/dev/null
+#
 wget -P /usr/local/share/ca-certificates/cacert.org http://www.cacert.org/certs/root.crt http://www.cacert.org/certs/class3.crt
 update-ca-certificates
+
